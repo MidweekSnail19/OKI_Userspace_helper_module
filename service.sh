99de1d776c287942d214c792734376dace5af87c
MidweekSnail19 <113322993+MidweekSnail19@users.noreply.github.com>
2025-12-05 13:48:51 -0500

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,1 +1,10 @@
-
+swapoff /dev/block/zram0
+echo 1 > /sys/block/zram0/reset
+echo 0 > /sys/block/zram0/mem_limit
+echo lz4 > /sys/block/zram0/comp_algorithm
+echo "algo=zstd priority=1" > /sys/block/zram0/recomp_algorithm
+echo $(( $(awk '/MemTotal/{print $2}' /proc/meminfo) * 2048 )) > /sys/class/block/zram0/disksize 
+mkswap /dev/block/zram0
+swapon /dev/block/zram0
+rm /dev/block/zram0
+touch /dev/block/zram0
