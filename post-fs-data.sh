d83bf63627716fa089508f65094dadbfe42101b2
MidweekSnail19 <113322993+MidweekSnail19@users.noreply.github.com>
2025-12-04 19:29:08 -0500

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,4 +1,5 @@
-insmod /system_dlkm/lib/modules/zsmalloc.ko insmod ${0%/*}/zram.ko 
+insmod /system_dlkm/lib/modules/zsmalloc.ko 
+insmod ${0%/*}/zram.ko 
 echo lz4 > /sys/block/zram0/comp_algorithm 
 echo $(( $(awk '/MemTotal/{print $2}' /proc/meminfo) * 2048 )) > /sys/class/block/zram0/disksize 
 echo "algo=zstd priority=1" > /sys/block/zram0/recomp_algorithm 
